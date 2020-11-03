d7f9012acaf91e90d5a44c4a65eaece0c059596d
Vitaliy Nishukov <vnishukov@gmail.com>
2020-11-03 17:46:51 +0300

    4

--- cvat-ui/src/components/annotation-page/annotation-filters/annotation-filters-pane/annotation-filters-pane.tsx
+++ cvat-ui/src/components/annotation-page/annotation-filters/annotation-filters-pane/annotation-filters-pane.tsx
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { Icon } from 'antd';
+import { Icon, Popconfirm } from 'antd';
 import React, { ReactElement, useRef, useState } from 'react';
 import AnnotationFiltersItem from '../annotation-filters-item/annotation-filters-item';
 import './style.scss';
@@ -10,9 +10,20 @@ import './style.scss';
 const AnnotationFilterPanel = (): ReactElement => {
     const [filters, setFilters] = useState([] as number[]);
     const filtersEndRef = useRef<null | HTMLDivElement>(null);
+    const clearFiltersRef = useRef<null | HTMLAnchorElement>(null);
 
     const scrollFiltersToBottom = (): void => {
         setTimeout(() => filtersEndRef?.current?.scrollIntoView({ block: 'center', behavior: 'smooth' }), 100);
+    };
+
+    const resetFilters = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+        e.preventDefault();
+        setFilters([]);
+    };
+
+    const confirmClearFilters = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+        e.preventDefault();
+        clearFiltersRef?.current?.click();
     };
 
     const openFilterPanel = (): void => {
@@ -20,22 +31,29 @@ const AnnotationFilterPanel = (): ReactElement => {
         scrollFiltersToBottom();
     };
 
-    const resetFilters = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
-        e.preventDefault();
-        setFilters([]);
-    };
-
     return (
         <div
             className='annotation-filters-pane'
             onClick={openFilterPanel}
-            onContextMenu={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => resetFilters(e)}
+            onContextMenu={(e: React.MouseEvent<HTMLElement, MouseEvent>) => confirmClearFilters(e)}
         >
             {filters?.length ? (
                 <>
                     {filters.map((item: number) => (
                         <AnnotationFiltersItem key={item} item={item} />
                     ))}
+                    <div className='pop-confirm-wrapper' onClick={(e) => e.stopPropagation()}>
+                        <Popconfirm
+                            placement='bottom'
+                            title='Are you sure you want to clear all filters?'
+                            icon={<Icon type='question-circle-o' style={{ color: 'red' }} />}
+                            onConfirm={(e) => resetFilters(e)}
+                            okText='Yes'
+                            cancelText='No'
+                        >
+                            <span ref={clearFiltersRef} />
+                        </Popconfirm>
+                    </div>
                     <div ref={filtersEndRef} />
                 </>
             ) : (
