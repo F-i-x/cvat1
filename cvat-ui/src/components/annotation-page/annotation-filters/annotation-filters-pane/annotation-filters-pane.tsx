6e110607b94fc0ea2bb37c5b04aa509933d403a6
Vitaliy Nishukov <vnishukov@gmail.com>
2020-11-03 16:22:03 +0300

    3

--- cvat-ui/src/components/annotation-page/annotation-filters/annotation-filters-pane/annotation-filters-pane.tsx
+++ cvat-ui/src/components/annotation-page/annotation-filters/annotation-filters-pane/annotation-filters-pane.tsx
@@ -3,15 +3,21 @@
 // SPDX-License-Identifier: MIT
 
 import { Icon } from 'antd';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useRef, useState } from 'react';
 import AnnotationFiltersItem from '../annotation-filters-item/annotation-filters-item';
 import './style.scss';
 
 const AnnotationFilterPanel = (): ReactElement => {
     const [filters, setFilters] = useState([] as number[]);
+    const filtersEndRef = useRef<null | HTMLDivElement>(null);
+
+    const scrollFiltersToBottom = (): void => {
+        setTimeout(() => filtersEndRef?.current?.scrollIntoView({ block: 'center', behavior: 'smooth' }), 100);
+    };
 
     const openFilterPanel = (): void => {
-        setFilters([...filters, Math.floor(Math.random() * 100000)]);
+        setFilters([...filters, Math.floor(Math.random() * 10)]);
+        scrollFiltersToBottom();
     };
 
     const resetFilters = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
@@ -26,7 +32,12 @@ const AnnotationFilterPanel = (): ReactElement => {
             onContextMenu={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => resetFilters(e)}
         >
             {filters?.length ? (
-                filters.map((item: number) => <AnnotationFiltersItem key={item} item={item} />)
+                <>
+                    {filters.map((item: number) => (
+                        <AnnotationFiltersItem key={item} item={item} />
+                    ))}
+                    <div ref={filtersEndRef} />
+                </>
             ) : (
                 <div className='no-filters'>
                     <Icon className='no-filters-icon' type='filter' />
